6dbce0e99bf015a629806ea53cae216900549568
Fabio Di Luca <fabio.diluca@gmail.com>
2020-06-24 16:04:01 -0300

    feat: integration test API 1

---
+++ src/Softplan.Desafio.Test/IntegrationTests/Api1ApplicationFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Softplan.Desafio.Test.IntegrationTests
+{
+    public class Api1ApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+
+    }
+}
---
+++ src/Softplan.Desafio.Test/IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Softplan.Desafio.Application.UseCases.TaxaJuros;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Softplan.Desafio.Test.IntegrationTests
+{
+    public class IntegrationTests :
+        IClassFixture<Api1ApplicationFactory<Api1.Startup>>
+    {
+        private readonly Api1ApplicationFactory<Api1.Startup>
+            _factory;
+
+        public IntegrationTests(
+            Api1ApplicationFactory<Api1.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Api1TaxaJurosMustBe1Percent()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("taxaJuros");
+            var taxaJuros = JsonConvert.DeserializeObject<TaxaJurosResponse>(await response.Content.ReadAsStringAsync());
+            Assert.Equal(0.01m, taxaJuros.TaxaJuros);
+        }
+
+    }
+}
+
